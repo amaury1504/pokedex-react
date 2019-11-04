241a3ef8f1f154a2b4f5c749aa1c1264ad880e59
Amaury Gimonnet <amaury.gimonnet@devinci.fr>
2019-11-04 23:16:12 +0100

    background and loader

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import { BrowserRouter } from 'react-router-dom';
 import NavBar from "./components/navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from "./components/Dashboard";
-
+import Background from "./backroung-pattern.jpg";
 
 class App extends Component{
   render(){
     return(
-      <BrowserRouter>
+      <div className={"App"} style={{background: `url(${Background})`}}>
         <NavBar/>
         <div className={"container"}>
             <Dashboard/>
         </div>
-      </BrowserRouter>
+      </div>
     )
   }
 }
